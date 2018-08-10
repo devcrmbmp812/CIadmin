734976fbf60cfa470846d6b03fceaf1eca49334f
devcrmbmp812 <andbel2323@gmail.com>
2018-08-10 22:27:23 +0800

    SMS logs CRUD

--- application/views/admin/include/sidebar.php
+++ application/views/admin/include/sidebar.php
@@ -141,6 +141,18 @@ $cur_tab = $this->uri->segment(2)==''?'dashboard': $this->uri->segment(2);
                   <li id="view_results" class=""><a href="<?= base_url('admin/results'); ?>"><i class="fa fa-circle-o"></i>View Results</a></li>
               </ul>
           </li>
+          <li id="smslogs" class="treeview">
+              <a href="#">
+                  <i class="fa fa-dashboard"></i> <span>Sms Logs</span>
+                  <span class="pull-right-container">
+                      <i class="fa fa-angle-left pull-right"></i>
+                  </span>
+              </a>
+              <ul class="treeview-menu">
+                  <li id="add_smslog"><a href="<?= base_url('admin/smslogs/add'); ?>"><i class="fa fa-circle-o"></i>Add Sms Logs</a></li>
+                  <li id="view_smslogs" class=""><a href="<?= base_url('admin/smslogs'); ?>"><i class="fa fa-circle-o"></i>View Sms Logs</a></li>
+              </ul>
+          </li>
           <li id="adminlte" class="treeview">
           <a href="#">
             <i class="fa fa-files-o"></i>
